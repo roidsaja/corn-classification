7cc1484c17447b3155c54f342d7ea2f08bcfb1e7
keitatofu <roidthebeast@gmail.com>
2022-05-06 17:44:33 +0300

    feat: added descriptions when detecting disease

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ body = """
 3. Common Rust 
 4. Cercospora Leaf Spot (Gray Leaf Spot)
 
-To get you started, you can use this image as an example by simply saving it into your device:
-![image](https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.N6mOMJ1HNBwMbii6QoexjgHaFj%26pid%3DApi&f=1)
+To get you started, you can use this gray leaf spot image as an example by simply saving it into your device:
+![image](https://cropwatch.unl.edu/2018-CW-News/2018-images/Disease/corn-gray-leaf-spot-backlit-2.jpg)
 
-[image source](https://www.ipmimages.org/browse/detail.cfm?imgnum=5465609)
+[image source](https://cropwatch.unl.edu/2018/differentiating-corn-leaf-diseases)
 """
 
 def predict(image):
@@ -46,10 +46,90 @@ def predict(image):
     }
 
     results = f"{classes[np.argmax(scores)]} with a { (100 * np.max(scores)).round(2)} % confidence score."
+
+    if classes[np.argmax(scores)] == 'Cercospora Leaf Spot (Gray Leaf Spot)':
+        st.markdown("""
+        ### Disease Facts
+        Gray leaf spot is caused by the fungus Cercospora zeae-maydis.
+
+        Epidemics of gray leaf spot have been observed in New York State in the Southern Tier and the Hudson River Valley. New hot spots of the disease have been reported in the Mohawk Valley and the Leatherstocking Region.
+
+        Gray leaf spot is favored by wet humid weather as often found in valley microclimates. Additionally, it is favored in situations with reduced tillage and continuous corn.
+
+        Airborne spores are spread locally and regionally from corn debris.
+
+        ### Symptoms
+
+        Symptoms of gray leaf spot are usually first noticed in the lower leaves.
+
+        Initially, lesions of gray leaf spot begin as a small dot with a yellow halo.
+
+        Lesions will elongate over time running parallel to the veins becoming pale brown to gray and rectangular in shape with blunt ends. These lesions can be described as having the appearance of a “matchstick.”
+
+        Lesions may eventually coalesce killing the leaves.
+
+        Leaves appear grayish in color due to the presence of fungal spores.
+
+        ### Management Strategies
+
+        Management strategies for gray leaf spot include tillage, crop rotation and planting resistant hybrids.
+
+        Fungicides may be needed to prevent significant loss when plants are infected early and environmental conditions favor disease.
+        """)
+
+    if classes[np.argmax(scores)] == 'Common Rust':
+        st.markdown("""
+        ### Disease Facts
+        Common rust is caused by the fungus Puccinia sorghi. Late occurring infections have limited impact on yield.
+
+        The fungus overwinters on plants in southern states and airborne spores are wind-blown to northern states during the growing season.
+
+        Disease development is favored by cool, moist weather (60 – 70◦ F).
+
+        ### Symptoms
+        Symptoms of common rust often appear after silking.
+
+        Small, round to elongate brown pustules form on both leaf surfaces and other above ground parts of the plant.
+
+        As the pustules mature they become brown to black.
+
+        If disease is severe, the leaves may yellow and die early.
+
+        ## Management Strategies
+        The use of resistant hybrids is the primary management strategy for the control of common rust.
+
+        Timely planting of corn early during the growing season may help to avoid high inoculum levels or environmental conditions that would promote disease development.
+        """)
+    
+    if classes[np.argmax(scores)] == 'Northern Leaf Blight':
+        st.markdown("""
+        ### Disease Facts
+        Northern corn leaf blight caused by the fungus Exerohilum turcicum is a common leaf blight found in New York.
+
+        If lesions begin early (before silking), crop loss can result. Late infections may have less of an impact on yield.
+
+        Northern corn leaf blight is favored by wet humid cool weather typically found later in the growing season.
+
+        Spores of the fungus that causes this disease can be transported by wind long distances from infected fields. Spread within and between fields locally also relies on wind blown spores.
+        
+        ### Symptoms
+        The tan lesions of northern corn leaf blight are slender and oblong tapering at the ends ranging in size between 1 to 6 inches.
+
+        Lesions run parallel to the leaf margins beginning on the lower leaves and moving up the plant. They may coalesce and cover the enter leaf.
+
+        Spores are produced on the underside of the leaf below the lesions giving the appearance of a dusty green fuzz.
+
+        ### Management Strategies
+        Northern corn leaf blight can be managed through the use of resistant hybrids.
+
+        Additionally, timely planting can be useful for avoiding conditions that favor the disease.
+        """)
+    
     return results
 
 
 def main():
+    st.sidebar.info('Survey Form: https://forms.gle/HFXHdhTN2K9oSnSM6')
     nav_select = st.sidebar.selectbox('Please select from the following', ['Classifier', 'Model Statistics and Review'])
 
     if nav_select == 'Model Statistics and Review':
@@ -73,8 +153,6 @@ def main():
         st.header('Confusion Matrix')
         st.image('assets/Confusion Matrix Model B0.png')
 
-        st.write('Survey Form: https://forms.gle/HFXHdhTN2K9oSnSM6')
-
 
     if nav_select == 'Classifier':
         st.title('Corn Plant Diseases Classifier')
@@ -85,6 +163,7 @@ def main():
         if file_upload is not None:
             for img in file_upload:
                 image = Image.open(img)
+                st.markdown('----')
                 st.image(image, caption='Image Preview', use_column_width=True)
                 if submit_button:
                     with st.spinner('Ongoing Classification...'):
