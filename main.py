2ecbfad480f3a47a884b08b5175e4dfd4f440f3e
keitatofu <roidthebeast@gmail.com>
2022-05-21 14:40:05 +0300

    refactor: include actual vs predicted diagram in streamlit

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import time
+import requests
 import streamlit as st
+
+from io import BytesIO
 from tensorflow.keras.models import load_model
 from tensorflow.keras import preprocessing
 from PIL import Image
-from pathlib import Path
+
 
 body = """
 #### The corn plant diseases are classified into four different classes: 
@@ -21,23 +24,24 @@ all extracted from [CALS Cornell website](https://cals.cornell.edu/field-crops/c
 
 ----
 
-To get you started, you can use these images by simply saving them in to your device:
+To get you started, a stock corn plant image has been reserved for you in the text field below. You can try out some other images too such as these:
 
 - [Gray Leaf Spot](https://cropwatch.unl.edu/2018-CW-News/2018-images/Disease/corn-gray-leaf-spot-backlit-2.jpg)
-- [Healthy](https://static2.bigstockphoto.com/8/6/1/large2/168470984.jpg)
+- [Norther Leaf Blight](https://cropwatch.unl.edu/2018-CW-News/2018-images/Disease/corn-northern-corn-leaf-blight.jpg)
 
-Image sources : [Bigstockphoto website](https://www.bigstockphoto.com/image-168470984/stock-photo-healthy-green-corn-leaves-growing-in-orchard-planting-corn-summer-corn-plant-image) and [Cropwatch UNL website](https://cropwatch.unl.edu/2018/differentiating-corn-leaf-diseases)
+Image sources : [Cropwatch UNL website](https://cropwatch.unl.edu/2018/differentiating-corn-leaf-diseases)
 """
 
 @st.experimental_memo(suppress_st_warning=True)
 def predict(_image):
     IMAGE_RES = (224, 224)
-    model = load_model('saved-model/best_model', compile=False)
+    model = load_model('saved-model/best_model')
     classes = ['Cercospora Leaf Spot (Gray Leaf Spot)', 'Common Rust', 'Healthy', 'Northern Leaf Blight']
     
-    test_images = _image.resize(IMAGE_RES)
-    test_images = preprocessing.image.img_to_array(test_images)
-    test_images = test_images / 255.0
+    test_images = tf.image.decode_jpeg(_image, channels=3)
+    test_images = tf.image.resize(test_images, IMAGE_RES)
+    # test_images = preprocessing.image.img_to_array(test_images)
+    # test_images = test_images / 255.0
     test_images = np.expand_dims(test_images, axis=0)
     # print(test_images)
 
@@ -159,12 +163,15 @@ def main():
         col1.image('assets/B0 Model Accuracy Plot History.png', caption='Accuracy Evolution Plot')
         col2.image('assets/B0 Model Loss Plot History.png', caption='Loss Evolution Plot')
 
+        st.header('Actual v. Predicted Diagram')
+        st.image('assets/Actual vs Predicted Diagram.png')
+
         st.header('Confusion Matrix')
         st.image('assets/Confusion Matrix Model B0.png')
 
         st.header('F1 Score')
         st.json("""
-        {"class_names":{"0":"Cercospora_leaf_spot Gray_leaf_spot","1":"Common_rust","2":"Healthy","3":"Northern_leaf_blight"},"f1_score":{"0":0.875,"1":0.9826989619,"2":0.9891696751,"3":0.9003215434}}
+{"class_names":{"0":"Cercospora_leaf_spot Gray_leaf_spot","1":"Common_rust","2":"Healthy","3":"Northern_leaf_blight"},"f1_score":{"0":0.9493670886,"1":0.9846153846,"2":1.0,"3":0.9644670051}}
         """)
 
 
@@ -174,22 +181,35 @@ def main():
         st.sidebar.success('You can now start classifying!')
         st.markdown(body)
         st.warning('It will take some time to classify, so please be patient.')
-        file_upload = st.file_uploader('Select an Image', type=['jpg', 'jpeg', 'png', 'gif'], accept_multiple_files=True)
+        # file_upload = st.file_uploader('Select an Image', type=['jpg', 'jpeg', 'png', 'gif'], accept_multiple_files=True)
+        # submit_button = st.button('Classify')
+
+        url_path = st.text_input('Enter Image URL To Classify...', value='https://static2.bigstockphoto.com/8/6/1/large2/168470984.jpg')
         submit_button = st.button('Classify')
-        if file_upload is not None:
-            for img in file_upload:
-                image = Image.open(img)
-                st.markdown('----')
-                st.image(image, caption='Image Preview', use_column_width=True)
-                if submit_button:
-                    with st.spinner('Generating Informative Descriptions...'):
-                        plt.imshow(image)
-                        plt.axis('off')
-                        predictions = predict(image)
-                        time.sleep(1)
-                    st.success('Image Has Been Classified')
-                    st.write(predictions)
-                predict.clear()
+        if submit_button:
+            content = requests.get(url_path).content
+            image = Image.open(BytesIO(content))
+            st.image(image, use_column_width=True)
+            predictions = predict(content)
+            time.sleep(1)
+            st.success('Image Has Been Classified')
+            st.write(predictions)
+        predict.clear()
+
+        # if file_upload is not None:
+        #     for img in file_upload:
+        #         image = Image.open(img)
+        #         st.markdown('----')
+        #         st.image(image, caption='Image Preview', use_column_width=True)
+        #         if submit_button:
+        #             with st.spinner('Generating Informative Descriptions...'):
+        #                 plt.imshow(image)
+        #                 plt.axis('off')
+        #                 predictions = predict(image)
+        #                 time.sleep(1)
+        #             st.success('Image Has Been Classified')
+        #             st.write(predictions)
+        #         predict.clear()
 
 if __name__ == '__main__':
     main()
